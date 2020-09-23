b372e4cdc32241f7bfee931b4e2a7ac48a0902f6
vivzha <vzha215@aucklanduni.ac.nz>
2020-09-23 19:33:45 +1200

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-// alert("hello");
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  description.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "1177d1f3b0df9e5dd2c9f2e91d6cae8d";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
